8e5e07f860e0ee3abf8b93e1510db708648e29e9
violinday <flwh123@sina.com>
2019-02-28 10:38:17 +0800

    support: as a sonar cli plugin

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/Kotlin.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/Kotlin.kt
@@ -9,7 +9,8 @@ const val NAME = "Kotlin"
 const val FILE_SUFFIX = ".kt"
 const val SCRIPT_SUFFIX = ".kts"
 
-const val DETEKT_WAY = "Detekt way"
+//增加 Soanr Cli支持。StandaloneActiveRulesProvider#createActiveRules#registerProfilesForLanguage  中对 Soanr way的过滤
+const val DETEKT_WAY = "Sonar way"
 const val DETEKT_SENSOR = "DetektSensor"
 const val DETEKT_REPOSITORY = "detekt-kotlin"
 const val DETEKT_ANALYZER = "Detekt-based Kotlin Analyzer"
--- src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/profiles/KotlinProfile.kt
@@ -9,15 +9,17 @@ import org.sonar.api.profiles.RulesProfile
 import org.sonar.api.rules.Rule
 import org.sonar.api.rules.RulePriority
 import org.sonar.api.utils.ValidationMessages
+import org.sonarsource.api.sonarlint.SonarLintSide
 
 /**
  * @author Artur Bosch
  */
+@SonarLintSide
 class KotlinProfile : ProfileDefinition() {
 
 	override fun createProfile(validation: ValidationMessages): RulesProfile {
 		val profile = RulesProfile.create(DETEKT_WAY, KEY)
-		ruleKeys.filter { it.active }.forEach {
+		ruleKeys.forEach {
 			val severity = severityTranslations[it.issue.severity]
 			val priority = RulePriority.valueOfString(severity)
 			profile.activateRule(Rule.create(it.repository(), it.rule()), priority)
